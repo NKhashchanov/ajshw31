839289d0d12ed7f9abce5bf3afd6e380b142c83d
Николай <37711307+NKhashchanov@users.noreply.github.com>
2019-12-10 14:04:11 +0300

    up

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,13 @@
 export default function lifeColor(data) {
-    let color = '';
-    if (data.health > 50) {
-        color = 'healthy';
-    } else if (data.health > 15 && data.health <=50) {
-        color = 'wounded';
-    } else if (data.health <= 15) {
-        color = 'critical';
-    }
-    return color;
+  let color = '';
+  if (data.health > 50) {
+    color = 'healthy';
+  } else if (data.health > 15 && data.health <= 50) {
+    color = 'wounded';
+  } else if (data.health <= 15) {
+    color = 'critical';
+  }
+  return color;
 }
 
-console.log(lifeColor({name: 'Маг', health: 10}));
+console.log(lifeColor({ name: 'Маг', health: 10 }));
